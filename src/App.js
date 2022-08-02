c7bf9ae89a9d0ef10eca52d2aa3ddc6dd7a28444
Just1c2 <hieuvtth2110005@fpt.edu.vn>
2022-08-02 10:23:09 +0700

    changed files

--- src/App.js
+++ src/App.js
@@ -1,1 +1,77 @@
+import React, { Component } from "react";
+import { useState } from "react";
 
+function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const operate = ["+", "-", "*", "/", "."];
+
+  const updateCalc = (value) => {
+    if (
+      (operate.includes(value) && calc === "") ||
+      (operate.includes(value) && operate.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+
+    if (!operate.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+
+  const createNum = () => {
+    const nums = [];
+
+    for (let i = 1; i < 10; i++) {
+      nums.push(
+        <button onClick={() => updateCalc(i.toString())} key={i}>
+          {i}
+        </button>
+      );
+    }
+    return nums;
+  };
+
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  };
+
+  const delLast = () => {
+    if (calc === "") {
+      return;
+    }
+
+    const value = calc.slice(0, -1);
+
+    setCalc(value);
+  };
+
+  return (
+    <div className="App">
+      <div className="calculator">
+        <div className="display">
+          {result ? <span>({result})</span> : ""} {calc || "0"}
+        </div>
+        <div className="operators">
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+          <button onClick={() => updateCalc("/")}>/</button>
+
+          <button onClick={delLast}>Del</button>
+        </div>
+        <div className="nums">
+          {createNum()}
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={() => updateCalc(".")}>.</button>
+
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
